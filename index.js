8f7a7cf489d7c5b827754dac46bc125c1090a53a
Kamya Domnick <domisemak@yahoo.co.uk>
2020-07-24 09:38:18 +0300

    cleaning the code

--- index.js
+++ index.js
@@ -4,28 +4,23 @@ var io = require('socket.io')(http);
 
 app.get('/', (req, res)=>{
     res.sendFile(__dirname + '/index.html');
-});
-var nsp = io.of('/my-namespace');
-nsp.on('connection', function(socket){
-    console.log('someone connected');
-    nsp.emit('hi', 'Hello everyone');
-});
 
-// var clients = 0;
-// const user1 = "Kamya"
-// const clientId = `Hey ${user1} welcome!`
-// io.on('connection', (socket) => {
-//     clients++;
-//     socket.emit('newclientconnect', {descrption: clientId});
-//     socket.broadcast.emit('newclientconnect', {descrption: clients + ' clients connected!'})
-//     socket.on('chat message', (msg) => {
-//       io.emit('chat message', msg);
-//     });
-//     socket.on('disconnect', function(){
-//         clients--;
-//         socket.broadcast.emit('newclientconnect',  {description: clients + ' clients connected!'} )
-//     })
-//   });
+
+var clients = 0;
+const user1 = "Kamya"
+const clientId = `Hey ${user1} welcome!`
+io.on('connection', (socket) => {
+    clients++;
+    socket.emit('newclientconnect', {descrption: clientId});
+    socket.broadcast.emit('newclientconnect', {descrption: clients + ' clients connected!'})
+    socket.on('chat message', (msg) => {
+      io.emit('chat message', msg);
+    });
+    socket.on('disconnect', function(){
+        clients--;
+        socket.broadcast.emit('newclientconnect',  {description: clients + ' clients connected!'} )
+    })
+  });
 
 http.listen(3000, ()=>{
     console.log('Listening on *:3000');
