fd125eb53c630951c25fad3d90344aec313811b0
Kamya Domnick <domisemak@yahoo.co.uk>
2020-07-27 15:03:51 +0300

    remove rest

--- restAPI/db.js
+++
@@ -1,8 +0,0 @@
-async function listDatabases(client){
-    databaseLists = await client.db().admin().listDatabases();
-
-    console.log("Databases:");
-    databaseLists.databases.forEach(db=>console.log(` -${db.name}`));
-};
-
-module.exports = listDatabases;
